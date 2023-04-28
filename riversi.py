24af33a05db0fdc3a2cc899a598cf0c81ff59805
jk1204020 <130888452+jk1204020@users.noreply.github.com>
2023-04-28 22:29:40 +0900

    Debug Fin
    アシスト機能は未実装

--- riversi.py
+++ riversi.py
@@ -2,17 +2,15 @@
 
 import tkinter             #tkinter:GUI
 
-
-
 #define
-FONTSIZE = ("",24)
 CELLSIZE = 48         #1ますのピクセル数
+FONTSIZE = ("",24)
 BOADDW = 8
 OFSX = 2 * CELLSIZE
 OFSY = 1 * CELLSIZE
-TYPE_NONE = 255
 TYPE_BLACK = 0
 TYPE_WHITE = 1
+TYPE_NONE = 255
 turn = TYPE_BLACK
 passcnt = 0
 endflag = False
@@ -42,24 +40,86 @@ def getpiece(pos):
         index = (pos[1]*BOADDW)+pos[0]
         return board[index]
 
+def initboard():
+        global turn, passcnt, endflag
+        for y in range(BOADDW):
+                for x in  range(BOADDW):
+#                        setpiece((x,y), TYPE_NONE)
+                        setpiece((x, y), TYPE_NONE)
+        setpiece((3, 3), TYPE_BLACK)
+        setpiece((4, 3), TYPE_WHITE)
+        setpiece((3, 4), TYPE_WHITE)
+        setpiece((4, 4), TYPE_BLACK)
+
+        turn = TYPE_BLACK
+        passcnt = 0
+        endflag = False
+        redraw()
+
+#click
+def canvas_click(event):
+        if endflag == True:
+                initboard()
+                return
+
+        if passcnt >0:
+                nextturn()
+                redraw()
+                return
+
+        x = int((event.x - OFSX) / CELLSIZE)
+        y = int((event.y - OFSY) / CELLSIZE)
+        pos = (x,y)
+
+        if isinside(pos)==False:
+                return
+        if turnnablepiece(pos, turn)==0:
+                return
+        for vectol in range(8):
+                loopcount = serch(pos,vectol,turn)
+                temppos = pos
+                for i in range(loopcount):
+                        temppos = moveposition(temppos, vectol)
+                        setpiece(temppos, turn)
+
+        setpiece(pos,turn)
+        nextturn()
+        redraw()
+
+
+def nextturn():
+        global passcnt, endflag, turn
+        turn ^= 1
+        empty = 0
+        for y in range(BOADDW):
+                for x in range(BOADDW):
+                        if getpiece((x,y)) == TYPE_NONE: empty+=1
+                        if turnnablepiece((x,y),turn)>0:
+                                passcnt=0
+                                return
+
+        if empty == 0:
+                endflag=True
+                return
+
+        passcnt += 1
+        if passcnt >=2:
+                endflag = True
+
+
 def moveposition(pos, vectol):
         x = pos[0] + vectable[vectol][0]
         y = pos[1] + vectable[vectol][1]
         return (x,y)
 
-def initboard():
-        global turn, passcnt, endflag
-        for y in range(BOADDW):
-                for x in  range(BOADDW):
-                        setpiece((x,y), TYPE_NONE)
-        setpiece((3, 3), TYPE_BLACK)
-        setpiece((4, 3), TYPE_WHITE)
-        setpiece((3, 4), TYPE_WHITE)
-        setpiece((3, 4), TYPE_BLACK)
-        passcnt = 0
-        endflag = False
-        redraw()
+def turnnablepiece(pos,num):
+        if getpiece(pos) != TYPE_NONE:
+                return 0
 
+        total = 0
+        for vectol in range(8):
+                total+=serch(pos,vectol,num)
+        return total
 
 def serch(pos, vectol, num):
         piece=0
@@ -85,90 +145,16 @@ def drawpiece(pos, num):
         ya = pos[1] * CELLSIZE + OFSY
         xb = xa + CELLSIZE
         yb = ya + CELLSIZE
+        #盤面
         canvas.create_rectangle(xa, ya, xb, yb, fill='Green', width=2)
         if num == TYPE_NONE: return
         d = int(CELLSIZE /10)
-        canvas.create_oval(xa + d, ya + d, xb - d, yb - d, fill=colortbl[num], width=2)
-
-'''
-for y in range(BOADDW):
-        for x in range(BOADDW):
-                drawpiece((x,y),getpiece((x,y)))
-
-piece = serch((3,3),2,TYPE_BLACK)
-msg = "Result:" + str(piece)
-canvas.create_text(288,456,text=msg,font=FONTSIZE)
-'''
-
-'''
-def drawrect(pos,color):
-        xa = pos[0] * CELLSIZE
-        ya = pos[1] * CELLSIZE
-        xb = xa + CELLSIZE
-        yb = ya + CELLSIZE
-        canvas.create_rectangle(xa,ya,xb,yb,fill=color,width=2)
-'''
-
-#click
-def canvas_click(event):
-        xa = event.x - OFSX
-        ya = event.y - OFSY
-        pos = (xa,ya)
-
-        if endflag == True:
-                initboard()
-                return
-
-        if passcnt >0:
-                nextturn()
-                redraw()
-                return
-
-        if isinside(pos)==False:
-                return
-        if turnnablepiece(pos, turn)==0:
-                return
-        for vectol in range(8):
-                loopcount = serch(pos,vectol,turn)
-                temppos = pos
-                for vectol in range(loopcount):
-                        temppos = moveposition(temppos, vectol)
-                        setpiece(temppos, turn)
-
-        setpiece(pos,turn)
-        nextturn()
-        redraw()
-
-def nextturn():
-        global passcnt, endflag, turn
-        turn ^= 1
-        empty = 0
-        for y in range(BOADDW):
-                for x in range(BOADDW):
-                        if getpiece((x,y)) == TYPE_NONE: empty+=1
-                        if turnnablepiece((x,y),turn)>0:
-                                passcnt=0
-                                return
-
-        if empty == 0:
-                endflag=True
-                return
-
-        passcnt += 1
-        if passcnt >=2:
-                endflag = True
-
-def turnnablepiece(pos,num):
-        if getpiece(pos) != TYPE_NONE:
-                return 0
-
-        total = 0
-        for vectol in range(8):
-                total+=serch(pos,vectol,num)
-        return total
+        canvas.create_oval(xa+d, ya+d, xb-d, yb-d, fill=colortbl[num], width=2)
+#        canvas.create_oval(xa+d, ya+d, xb-d, yb-d, fill='Blue', width=2)
 
 def redraw():
-        canvas.create_rectangle(0,0,576,480,fill="Khaki")
+        #外枠
+        canvas.create_rectangle(0,0,576,480,fill="Khaki1")
         black=0
         white=0
 
@@ -178,45 +164,26 @@ def redraw():
                         num = getpiece(pos)
                         if num == TYPE_BLACK: black+=1
                         if num == TYPE_WHITE: white+=1
-                        drawpiece(pos,turn)
+                        drawpiece(pos,num)
                         #assist(pos,turn)
         msg = "黒"+str(black) + "　対　白" + str(white)
         canvas.create_text(288, 456, text=msg, font=FONTSIZE)
         msg = prayertbl[turn]+"の番です"
         if passcnt>0:
                 msg += "(パス)"
-
         if endflag == True:
                 msg = "終了です"
 
         canvas.create_text(288, 24, text=msg, font=FONTSIZE)
 
-'''
-        canvas.create_rectangle(xa,ya,xb,yb,fill='Green',width=2)
-        d = int(CELLSIZE / 10)
-        canvas.create_oval(xa+d, ya+d, xb-d, yb-d, fill='White', width=2)
-
-
-#canvas.bind("<Button-1>", canvas_click)
-
-startpos = (5,5)
-drawrect(startpos, 'Red')
-for vec in range(8):
-        temppos = startpos
-        for i in range(4):
-                temppos = moveposition(temppos,vec)
-                drawrect(temppos, 'Cyan')
-
-'''
 #windiw
 root = tkinter.Tk()        # make window
+root.title("Riversi")
 root.geometry("576x480")  # window Size
-root.title("Reversi")
 
 #canvas
 canvas = tkinter.Canvas(root, width=576, height=480)
 canvas.pack()
 canvas.bind("<Button-1>",canvas_click)
 initboard()
-
 root.mainloop()           #show window
